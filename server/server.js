b1e3907bbbaf7768df6425974c3819e4245ee07d
sulemankundi4 <msulemankdi@gmail.com>
2025-05-05 13:31:16 +0500

    another fix

--- server/server.js
+++ server/server.js
@@ -27,14 +27,14 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 
-const corsOptions = {
-  origin: ["https://smart-dorm-hostel-management-c3av.vercel.app", "http://localhost:5174"],
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: ["https://smart-dorm-hostel-management-c3av.vercel.app", "http://localhost:5174"],
+//   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+//   credentials: true,
+// };
 
-app.use(cors(corsOptions));
+app.use(cors()); // This allows all origins
 
 mongoose
   .connect(process.env.MONGO_URI)
