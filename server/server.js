f635efebc9a6c323d1dc57483f7f1f708308c6ac
sulemankundi4 <msulemankdi@gmail.com>
2025-05-05 13:27:44 +0500

    server changes

--- server/server.js
+++ server/server.js
@@ -28,8 +28,10 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 const corsOptions = {
-  origin: "https://smart-dorm-hostel-management-c3av.vercel.app", // replace with your client's origin
-  credentials: true, // this allows the cookie to be sent with the request
+  origin: ["https://smart-dorm-hostel-management-c3av.vercel.app", "http://localhost:5174"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
